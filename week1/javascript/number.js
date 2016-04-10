1160233b201e5ebb4a51b2d6dca90603ab883ba4
tomlan <tomlan999@outlook.com>
2016-04-10 11:13:15 -0700

    Week1 HW - input form with number calculations

---
+++ week1/javascript/number.js
@@ -0,0 +1,44 @@
+// Create variables
+var submit = document.getElementById("submit"); //variable representing submit button
+var reset = document.getElementById("reset"); //variable representing reset button
+var totalnumbers = 0;
+var sum = 0;
+var average = 0;
+
+// Create event listeners - when click event occurs, execute function
+submit.addEventListener("click", onClick);
+reset.addEventListener("click", reset);
+
+// Submit button function
+function onClick(evt) {
+  evt.preventDefault(); // Prevent form from submitting
+  var element = $( "#number" ).val(); // Retrieve value submitted to the form
+  if ($.isNumeric(element) == true) { // Check to see if value is numeric
+    totalnumbers++; // Add one to submission count
+    var element1 = Number(element); // Convert value to numeric value
+    sum = (sum + element1); // Add to sum of all numbers
+    average = (sum / totalnumbers); // Calculate new average
+    // Update all paragraph elements
+    document.getElementById("totalnumbers").innerHTML = totalnumbers;
+    document.getElementById("sum").innerHTML = sum;
+    document.getElementById("average").innerHTML = average;
+  }
+  else {
+    alert ("Please enter a numeric value!!");
+  }
+  $( "#number" ).val(""); // Clear the input field
+}
+
+// Reset button function
+function reset(evt){
+  evt.preventDefault(); 
+  $( "#number" ).val(""); // Clear the input field
+  // Reset variables
+  totalnumbers = 0;
+  sum = 0;
+  average = 0;
+  // Reset all paragraph elements
+  document.getElementById("totalnumbers").innerHTML = "0";
+  document.getElementById("sum").innerHTML = "0";
+  document.getElementById("average").innerHTML = "0";
+}
